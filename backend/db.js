c0b1dc0e84b2445a1ac9dc5b4552733d4e0b3935
MAT-375 <ahsantariq1996@gmail.com>
2023-06-11 05:30:14 -0700

    slight adjustments and project expanded

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true&tls=false&readPreference=primary"
+
+
+const connectToMongo = async () => {
+    await mongoose.connect(mongoURI);
+    console.log("connected successfully");
+}
+
+module.exports = connectToMongo;
